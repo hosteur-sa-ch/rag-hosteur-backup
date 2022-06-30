d8b48465322498e061f62e2748d2b6b6d7a69247
hugo tritz <git.perso@hugotritz.net>
2022-06-30 09:28:12 +0200

    prune

--- scripts/install-restic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f /etc/redhat-release ] ; then
-    yum install yum-plugin-copr
-    yum copr enable copart/restic
-    yum install restic
-elif [ -f /etc/debian_version ] ; then
-    apt-get install restic
-fi
